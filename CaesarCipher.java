197c26f838094b016f48f26661b4b42fc992a410
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-02-21 14:30:04 +0000

    i tried

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -13,24 +13,56 @@ public class CaesarCipher {
 	private char[] shifted;
 	private int shift;
 
-    public CaesarCipher() {
+    //whatever the shift number is, use mod for bigger numbers
 
+    public CaesarCipher() {
+        //give shift a default value that isnt 0 or over 25
+        shift = 2;
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     }
 
     public CaesarCipher(int num){
-
+        //populated shifted array with alphabet shifted
+        num = shift;
+        //shifted = {'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b'};
+        for(int i=0; i<alphabet.length; i++)
+        {
+            shifted[i] = alphabet[i+num];
+        }
+       
     }
 
     public String encrypt(String message){
+        //call to shifter
+        //look through the alphabet
+        int apos=0;
+        int cry=0;
+        for(int i=0; i <message.length(); i++)
+        {
+            apos = message.indexOf(i);
+        }
+        cry = apos + 2;
 
+        for(int i=0; i<message.length(); i++)
+        {
+            message.charAt(i) = alphabet[cry];
+        }
+
+        //concatonate
+        //going through each character in array until
+        //if it isnt found it is just concatenated in its original state, booleans.
+        return ""+;
     }
 
     public String decrypt(String message){
-
+        //call to shifter
+        //look through the shifted
+        message.charAt() = 
+        return ""+;
     }
 
     public void shifter(int num){
-
+        //shift over num spaces
     }
 
 
--- G19.java
+++ G19.java
@@ -15,7 +15,7 @@ public class G19 {
 
     	System.out.println(cipher.encrypt("hello world!"));
     	System.out.println(cipher.decrypt("ukq zez ep!"));
-
+	
     	//Create a new cipher using the shifter(#) method
     	//Encrypt and decrypt a message with your new cipher
 
